7d7cc43a1f3e45d4da7f116b478f843cf206392e
Eric Crain <ericcrain77@gmail.com>
2022-01-16 12:32:11 -0500

    adding typedefs resolvers and middleware to server file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
-const routes = require('./routes');
+const { start } = require('repl');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  // create new server and pass schemas through
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
+  // start server
+  await server.start();
+
+  // integrate middleware
+  server.applyMiddleware({ app });
+
+  // log GraphQL Query Server
+  console.log(`Use GraphQL to test queries and mutations at http://localhost:${PORT}/${server.graphqlPath}`)
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +40,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
