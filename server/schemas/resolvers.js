ac76fa907564cbb67ccb451dbf0224e17cf512ab
Eric Crain <ericcrain77@gmail.com>
2022-01-16 12:03:07 -0500

    adding query and mutations to resolvers file

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,68 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('User Not Logged In');
+        }
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+            const correctPw = await user.isCorrectPassowrd(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(args);
+
+            return { token, user };
+        },
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const addBook = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: book } },
+                    { new: true }
+                );
+
+                return addBook;
+            }
+
+            throw new AuthenticationError('Please Log In')
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const removeBook = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                );
+
+                return removeBook;
+            }
+
+            throw new AuthenticationError('Please Log In');
+        }
+    }
+};
+
+module.exports = resolvers;
