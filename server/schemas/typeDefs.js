363fe85837dffc62f8b43cabbcfe8f68f42fcd31
Eric Crain <ericcrain77@gmail.com>
2022-01-16 22:14:28 -0500

    refactoring auth.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,11 +34,14 @@ const typeDefs = gql`
 
     type Query {
         me: User
+        users: [User]
+        user(username: String!): User
+        
     }
 
     type Mutation {
-        login(email: String!, password: String!): User
-        addUser(username: String!, email: String!, password: String!): User
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
         saveBook(book: BookInput!): User
         removeBook(bookId: ID!): User
     }
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -15,24 +15,18 @@ module.exports = {
       token = token.split(' ').pop().trim();
     }
 
-    if (!token) {
-      return req;
-    }
-
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return req;
     }
 
     return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
