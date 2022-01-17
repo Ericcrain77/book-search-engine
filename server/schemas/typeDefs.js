fc9911036e6f98fd8ca3c6e64dbd2ed5821a1291
Eric Crain <ericcrain77@gmail.com>
2022-01-16 22:01:20 -0500

    refactoring typeDefs file to fix sign up

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -37,8 +37,8 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): User
+        addUser(username: String!, email: String!, password: String!): User
         saveBook(book: BookInput!): User
         removeBook(bookId: ID!): User
     }
