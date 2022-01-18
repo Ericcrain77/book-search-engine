a4ac2721d502d1f7d654684b88adcc1628024155
Eric Crain <ericcrain77@gmail.com>
2022-01-17 21:52:35 -0500

    debugging

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -7,6 +7,10 @@ import { ADD_USER } from '../utils/mutations.js';
 function SignupForm(props) {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
